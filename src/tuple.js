f60927359205382c6b05fd8a598812d444020a43
j-te <34647206+j-te@users.noreply.github.com>
2021-12-23 13:34:42 +0000

    removed lodash from tuple.js

--- src/tuple.js
+++ src/tuple.js
@@ -1,15 +1,14 @@
 import {concat} from "./core.js";
 import {register} from "./coder.js";
-import _ from "lodash";
 
 export function tuple(entries) {
     return {
         encode: function (array) {
-            return concat(_.map(entries, (entry, i) => entry.encode(array[i])));
+            return concat(entries.map((entry, i) => entry.encode(array[i])));
         },
         decode: function (rest) {
             var array = [];
-            _.each(entries, (entry, i) => {
+            entries.forEach((entry, i) => {
                 var result = entry.decode(rest);
                 array[i] = result.value;
                 rest = result.rest;
